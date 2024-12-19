323c5317df364d03bad6448200521666d9846075
Prashant V Gaikar <1729prashant@gmail.com>
2024-12-19 19:45:15 +0530

    updated main.go to check parameters passed

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
-	fmt.Println("Hello, World!")
+	// Check if exactly one command-line argument (besides the program name) is provided
+	if len(os.Args) < 2 {
+		fmt.Println("no website provided")
+		os.Exit(1)
+	} else if len(os.Args) > 2 {
+		fmt.Println("too many arguments provided")
+		os.Exit(1)
+	}
+
+	// If we have exactly one argument, it's the BASE_URL
+	baseURL := os.Args[1]
+	fmt.Printf("starting crawl of: %s\n", baseURL)
 }
